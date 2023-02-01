de0ececd7eca670902c9a8dda8571a6b5bf348b1
giuseppemaisto <gmaisto98@gmail.com>
2023-02-01 21:12:39 +0100

    inserita lista

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data(){
         return{
             apiUrl: 'server.php',
             todoList: [],
+            language: '',
         }
     },
+    methods: {
+        addToDo(){
+            const data = {
+                todoItem: this.language
+            }
+            axios.post(this.apiUrl, data, {
+                headers: {'Content-type': 'multipart/form-data'}
+            }).then((response) => {
+                this.language = '';
+                this.todoList = response.data;
+            })
 
-    
+
+        }
+    },
+    mounted() {
+        axios.get(this.apiUrl).then((response) =>{
+            this.todoList = response.data;
+        });
+    }
+
+
 }).mount('#app');
--- server.php
+++ server.php
@@ -1,11 +1,26 @@
 <?php 
     //Recupero del contenuto di todo-list.json
-    $string = file_get_contents('todo_list.json');
+    $string = file_get_contents('todo-list.json');
 
       //Convertire la stringai in array associativo
-      $todolist = json_decode($string, true);
+      $todo_list = json_decode($string, true);
+
+      if(isset($_POST['todoItem'])){//se c'è valore post allora avviene 
+        $todo_item = $_POST['todoItem'];//passo il valore di $_POST di una nuova variabile 
+        
+        $todo_array =[
+            "language" => $todo_item,
+            "done" => false,
+        ];
+        $todo_list[] = $todo_array;
+        //Aggiungo un nuovo elemento nell'array
+        // array_push($todo_list, $todo_item);
+        //Scrivo il dato all'interno del file json
+        file_put_contents('todo-list.json', json_encode($todo_list));
+    }
+
 
       header ('content-Type: application/json');
 
-      echo json_encode($todolist);
+      echo json_encode($todo_list);
 ?>
